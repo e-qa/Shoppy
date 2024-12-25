c8fc549d7b372189c0db0270c6a3ce254d1658d6
e-qa <eliqarayev835@gmail.com>
2024-12-25 17:48:26 +0400

    deploy issues

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,39 +1,18 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
 import { GetSingleProduct } from '@/utils/actions';
 import Image from 'next/image';
 
-const SingleProductPage = async ({ params }: { params: { id: string } }) => {
-  const product = await GetSingleProduct(params.id);
-  const { name, image, company, description, price } = product;
+const SingleProductPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const resolvedParams = await params;
+  const product = await GetSingleProduct(resolvedParams.id);
 
-  if (!product) {
-    <h1>not found</h1>;
-  }
+  const { name, image, company, description, price } = product;
 
   return (
     <section>
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/">Home</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/products">Products</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
       <div className="mt-6 grid gap-y-8 grid-cols-2 lg:gap-x-16">
         <div className="relative">
           <Image
@@ -46,20 +25,12 @@ const SingleProductPage = async ({ params }: { params: { id: string } }) => {
           />
         </div>
         <div>
-          <div className="flex gap-x-8 items-center">
-            <h1 className="capitalize text-3xl font-bold">{name} </h1>
-            <div className="flex items-center gap-x-2">
-              {/* <FavoriteToggleButton productId={params.id} /> */}
-              {/* <ShareButton name={product.name} productId={params.id} /> */}
-            </div>
-          </div>
-          {/* <ProductRating productId={params.id} /> */}
+          <h1 className="capitalize text-3xl font-bold">{name}</h1>
           <h4 className="text-xl mt-2">{company}</h4>
           <p className="mt-3 text-md bg-muted inline-block p-2 rounded">
             {price}$
           </p>
           <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
-          {/* <AddToCart productId={params.id} /> */}
         </div>
       </div>
     </section>
